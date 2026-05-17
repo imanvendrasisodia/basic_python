f56df3a7080542d7649e7e736abc9ec19b13703a
imanvendrasisodia <imxmanvendra@gmail.com>
2026-05-17 23:47:57 +0530

    find percentage

--- in.c
+++ in.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello, World!");
+    printf("hello mr ansh\n");
+    printf(" enter your marks all subjects\n");
+    float percentage, marks;
+    scanf("%f", &marks);
+    percentage = ((marks / 500.0) * 100);
+printf("your percentage is %f", percentage);
     return 0;
 }
