27a732150dc4e333f41af3253ff17f480033f6a5
imanvendrasisodia <imxmanvendra@gmail.com>
2026-05-18 15:12:58 +0530

    update

--- in.c
+++ in.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    printf("hello mr ansh\n");
+    printf("hello user:\n");
     printf(" enter your marks all subjects\n");
     float percentage, marks;
     scanf("%f", &marks);
     percentage = ((marks / 500.0) * 100);
 printf("your percentage is %f", percentage);
     return 0;
-    getch();
+    
 }
