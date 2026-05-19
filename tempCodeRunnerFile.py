911ddb0ac3ff12472cd8f341908b731e9780ca0c
imanvendrasisodia <imxmanvendra@gmail.com>
2026-05-20 02:31:10 +0530

    delete unwanted file

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-le in cm: "))
-width = float(input("Enter the width of rectangle in cm: "))
-area = length * width
-print("he area of rectangle is : ", area, "cm^2")
-print("Thank you for using our area find calculator Mr. ", name)
