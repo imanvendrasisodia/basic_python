3986d40cc79bfdfff9f90b42c3855cd23b2a3b2c
imanvendrasisodia <imxmanvendra@gmail.com>
2026-05-20 02:15:45 +0530

    circearea find

---
+++ area_of_circle.py
@@ -0,0 +1,7 @@
+# area of circle
+name = input ("Enter your good name:")
+radius = float(input("Enter the radius of circle:"))
+pi = 3.14
+area = (radius* radius) *pi
+print("area of circle : ", area)
+print("Thank you for using our area find calcculator Mr. ", name)
