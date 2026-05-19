7b5a382a7cc8f19cdc99971f7af094d5d744005e
imanvendrasisodia <imxmanvendra@gmail.com>
2026-05-20 02:24:42 +0530

    area of rectangle

---
+++ area_of_rectangle.py
@@ -0,0 +1,6 @@
+name = input("Enter your good name sir: ")
+length = float(input("Enter the lenght of rectangle in cm:"))
+width = float(input("Enter the width of rectangle in cm: "))
+area = length * width
+print("he area of rectangle is : ", area, "cm^2")
+print("Thank you for using our area find calculator Mr. ",name)
